519bd10d52f48e0c1db5790821a3075e35c94992
Vindhya Prakash <vindhyaprakash11@gmail.com>
2022-02-02 20:59:03 +0530

    connected to firebase

--- app/src/main/java/com/example/ekyc_playground/Register.kt
+++ app/src/main/java/com/example/ekyc_playground/Register.kt
@@ -2,11 +2,15 @@ package com.example.ekyc_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
-    //var TAG:String = Register::class.java.simpleName
-
+    var TAG:String = Register::class.java.simpleName
+    val db = Firebase.firestore
     lateinit var Name: EditText
     lateinit var PostalAddress: EditText
     lateinit var Pan: EditText
@@ -31,7 +35,24 @@ class Register : AppCompatActivity() {
         }
     }
     private fun registerUser() {
+        val user = hashMapOf(
+            "Name" to Name.text.toString(),
+            "Address" to PostalAddress.text.toString(),
+            "Pan" to Pan.text.toString(),
+            "Aadhar" to aadhar.text.toString(),
+            "Email" to email.text.toString()
+        )
 
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+        Toast.makeText(this,"Registered", Toast.LENGTH_SHORT).show()
     }
 }
 
