d67dc8b2b40dd52fda825a5cce8542d6d4ff15f3
Vindhya Prakash <vindhyaprakash11@gmail.com>
2022-02-03 23:16:44 +0530

    validation

--- app/src/main/java/com/example/ekyc_playground/Register.kt
+++ app/src/main/java/com/example/ekyc_playground/Register.kt
@@ -1,12 +1,23 @@
 package com.example.ekyc_playground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import android.text.TextUtils
+import android.util.Patterns
+
+
+
+
+
+
 
 class Register : AppCompatActivity() {
     var TAG:String = Register::class.java.simpleName
@@ -16,6 +27,7 @@ class Register : AppCompatActivity() {
     lateinit var Pan: EditText
     lateinit var aadhar: EditText
     lateinit var email: EditText
+    lateinit var register: Button
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -25,8 +37,48 @@ class Register : AppCompatActivity() {
         Pan = findViewById(R.id.pan)
         aadhar = findViewById(R.id.aadhar)
         email = findViewById(R.id.EmailAddress)
+        register = findViewById(R.id.btnRegister)
+
+        register.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                checkDataEntered()
+            }
+
+            private fun checkDataEntered() {
+
+                if (isEmpty(Name)) {
+                    Name.setError("Name is required!");
+                }
+                if (isEmpty(Pan)) {
+                    Pan.setError("Pan is required!");
+                }
+                if (isEmpty(PostalAddress)) {
+                    PostalAddress.setError("Address is required!");
+                }
+                if (isEmpty(aadhar)) {
+                    aadhar.setError("Aadhar is required!");
+                }
+                if (isEmail(email) == false) {
+                    email.setError("Enter valid email!");
+                }
+            }
+
+            fun isEmpty(text: EditText): Boolean {
+                val str: CharSequence = text.text.toString()
+                return isEmpty(str)
+            }
+
+            fun isEmail(text: EditText): Boolean {
+                val email: CharSequence = text.text.toString()
+                return !isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+
+        })
 
     }
+
+
+
     fun clickHandler(view: android.view.View) {
         when (view.id) {
             R.id.btnRegister -> {
